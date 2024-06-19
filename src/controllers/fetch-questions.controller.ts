1a6bd91c12d9e1e595bddc3f8bdba9b948a2a067
bellettati <bellettati28@gmail.com>
2024-06-19 17:45:43 -0300

    fetch questions controller

---
+++ src/controllers/fetch-questions.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Query, UseGuards } from '@nestjs/common'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const pageQuerySchema = z
+    .string()
+    .optional()
+    .default('1')
+    .transform(Number)
+    .pipe(z.number().min(1))
+
+type PageQuerySchema = z.infer<typeof pageQuerySchema>
+
+const queryValidationPipe = new ZodValidationPipe(pageQuerySchema)
+
+@Controller('/questions')
+export class FetchQuestionsController {
+    constructor(private prisma: PrismaService) {}
+
+    @Get()
+    @UseGuards(JwtAuthGuard)
+    async handle(@Query('page', queryValidationPipe) page: PageQuerySchema) {
+        const questions = await this.prisma.question.findMany({
+            skip: (page - 1) * 20,
+            take: page * 20,
+        })
+
+        return { questions }
+    }
+}
